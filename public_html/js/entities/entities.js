ebb9448562ea364bb0380444b455db4eee748b19
Stephanie Nando <stephaniesnando@gmail.com>
2014-11-14 11:25:40 -0800

    Day 3

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -10,8 +10,12 @@ game.PlayerEntity = me.Entity.extend({
                 getShape: function (){
                     return (new me.Rect(0, 0, 128, 128)).toPolygon();
                 }
-       
-        }]);
+       }]);
+    
+        this.renderable.addAnimation("idle", [3]);
+        this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
+        
+        this.renderable.setCurrentAnimation("idle");
         
         this.body.setVelocity(5, 20);
     },
@@ -19,10 +23,21 @@ game.PlayerEntity = me.Entity.extend({
     update: function(delta) {
         if(me.input.isKeyPressed("right")){
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            
         }else{
             this.body.vel.x = 0;
         }
+        
+        if (this.body.vel.x !== 0) {
+            if (!this.renderable.isCurrentAnimation("smallWalk")) {
+                this.renderable.setCurrentAnimation("smallWalk");
+            }
+        }else{
+            this.renderable.setCurrentAnimation("idle");
+        }
+        
         this.body.update(delta);
+        this._super(me.Entity, "update", [delta] )
         return true;
     }
     
