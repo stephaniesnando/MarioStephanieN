260dbe0208b80172982849c9e093fdb294525d86
Stephanie Nando <stephaniesnando@gmail.com>
2014-11-20 11:27:02 -0800

    Day 5

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -18,6 +18,7 @@ game.PlayerEntity = me.Entity.extend({
         this.renderable.setCurrentAnimation("idle");
         
         this.body.setVelocity(5, 20);
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
     
     update: function(delta) {
@@ -40,28 +41,29 @@ game.PlayerEntity = me.Entity.extend({
             this.renderable.setCurrentAnimation("idle");
         }
 
-        this._super(me.Entity, "update", [delta] )
+        this._super(me.Entity, "update", [delta] );
         return true;
     },
     
     collideHandler: function(response){
-        
     }
     
     
 });
 
 game.LevelTrigger = me.Entity.extend({
-    
     init: function(x, y, settings){
+        console.log("Trigger");
         this._super(me.Entity, 'init', [x, y, settings]);
-        this.body.onCollison = this.onCollision.bind(this);
+        this.body.onCollision = this.onCollision.bind(this);
         this.level = settings.level;
         },
     
     onCollision: function(){
-        this.body.setCollisonMask(me.collisions.type.NO_OBJECT);
+        console.log("BOOOOM!!!@!@!!!!");
+        this.body.setCollisionMask(me.collision.types.NO_OBJECT);
         me.levelDirector.loadLevel(this.level);
+        me.state.current(). resetPlayer();
     }
     
 });
