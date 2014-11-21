eb175c429f1d0b0ac8395f1b8eb63293e5e450af
Stephanie Nando <stephaniesnando@gmail.com>
2014-11-21 11:27:35 -0800

    Day 6

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -8,7 +8,7 @@ game.PlayerEntity = me.Entity.extend({
                 width: 128,
                 height: 128,
                 getShape: function (){
-                    return (new me.Rect(0, 0, 128, 128)).toPolygon();
+                    return (new me.Rect(0, 0, 30, 128)).toPolygon();
                 }
        }]);
     
@@ -57,13 +57,15 @@ game.LevelTrigger = me.Entity.extend({
         this._super(me.Entity, 'init', [x, y, settings]);
         this.body.onCollision = this.onCollision.bind(this);
         this.level = settings.level;
+        this.xSpawn = settings.xSpawn;
+        this.ySpawn = settings.ySpawn;
         },
     
     onCollision: function(){
         console.log("BOOOOM!!!@!@!!!!");
         this.body.setCollisionMask(me.collision.types.NO_OBJECT);
         me.levelDirector.loadLevel(this.level);
-        me.state.current(). resetPlayer();
+        me.state.current(). resetPlayer(this.xSpawn, this.ySpawn);
     }
     
 });
