7ba4a103d7361021303eedf4d8640447914625fd
Stephanie Nando <stephaniesnando@gmail.com>
2014-12-08 20:35:03 -0800

    ..

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -13,11 +13,14 @@ game.PlayerEntity = me.Entity.extend({
        }]);
     
         this.renderable.addAnimation("idle", [3]);
+        this.renderable.addAnimation("bigIdle", [19]);
         this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
         this.renderable.addAnimation("jump", [5, 5]);
+        this.renderable.addAnimation("bigWalk", [14, 15, 16, 17, 18, 19], 80);
         
         this.renderable.setCurrentAnimation("idle");
         
+        this.big = false;
         this.body.setVelocity(5, 20);
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
@@ -52,6 +55,7 @@ game.PlayerEntity = me.Entity.extend({
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
         
+        if(!this.big){
         if (this.body.vel.x !== 0) {
             if (!this.renderable.isCurrentAnimation("smallWalk")) {
                 this.renderable.setCurrentAnimation("smallWalk");
@@ -61,6 +65,16 @@ game.PlayerEntity = me.Entity.extend({
         }else{
             this.renderable.setCurrentAnimation("idle");
         }
+    }else{
+        if (this.body.vel.x !== 0) {
+            if (!this.renderable.isCurrentAnimation("bigWalk")) {
+                this.renderable.setCurrentAnimation("bigWalk");
+                this.renderable.setAnimationFrame();
+                
+            }else{
+                this.renderable.setCurrentAnimation("bigIdle");
+            }
+    
 
 
         this._super(me.Entity, "update", [delta] );
@@ -76,6 +90,10 @@ game.PlayerEntity = me.Entity.extend({
                 response.b.alive = false;
             }else{
                 me.state.change(me.state.MENU);
+        }else if(response.b.type === 'mushroom'){
+            this.big = true;
+            me.game.world.removeChild(response.b);
+          console.log("!Big");  
         }
     }
 }
@@ -170,4 +188,24 @@ game.BadGuy = me.Entity.extend({
        
    }
     
+});
+
+game.Mushroom = me.Entity.extend({
+        init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+                image: "mushroom",
+                spritewidth: "64",
+                spriteheight: "64",
+                width: 64,
+                height: 64,
+                getShape: function() {
+                    return (new me.Rect(0, 0, 64, 64)).toPolygon();
+                }
+            }]);
+        
+        me.collision.check(this);
+        this.type = "mushroom";
+        
+    }
+
 });
--- public_html/js/game.js
+++ public_html/js/game.js
@@ -41,6 +41,7 @@ var game = {
 	"loaded" : function () {
                 me.pool.register("mario", game.PlayerEntity, true);
                 me.pool.register("BadGuy", game.BadGuy);
+                me.pool.registere("mushroom", game.Mushroom);
                 
                 me.pool.register("levelTrigger", game.LevelTrigger);
             
